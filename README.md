06061fc6dbaa82b03cf9e231175b927320d27100
hrj2233 <hrj2233@gmail.com>
2022-07-20 01:53:36 +0900

    Recreate rock paper scissors in main.js

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # rock-paper-scissors
 
- - The rules require that competing players use one hand to form one of three shapes at an agreed-upon time. 
- - The person that plays the strongest “object” is the winner of the game.
- - rock > scissors , paper > rock, scissors > paper. 
- - If both players choose the same shape, the game is tied.
+자바스크립트로 만든 가위 바위 보
--- main.js
+++ main.js
@@ -1,40 +1,55 @@
-function computerPlay() {
-    const rps = ["Rock", "Paper", "Scissors"];
-    const rpsNum = Math.floor(Math.random() * 3);
-    return rps[rpsNum];
-}
-
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-    const player = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if (player === computerSelection) {
-        console.log("it's tie");
-        return "it's tie";
-    } else if (player === "Rock" && computerSelection === "Paper" || player === "Paper" && computerSelection === "Scissors" || player === "Scissors" && computerSelection === "rock") {
-        computerScore++;
-        console.log(`You Lose! ${computerSelection} beats ${player}`);
-        return `You Lose! ${computerSelection} beats ${player}`;
-    } else {
+const getComputerChoice = () => {
+    const choice = Math.floor(Math.random() * 3);
+    let result;
+    switch (choice) {
+        case 0:
+            result = '가위';
+            break;
+        case 1:
+            result = '바위';
+            break;
+        case 2:
+            result = '보';
+            break;
+        default:
+            console.log('가위 바위 보가 생성 안됨.');
+    }
+    return result;
+};
+
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        console.log('비김', `나:${playerSelection} 컴퓨터:${computerSelection}`);
+    } else if (playerSelection === '가위' && computerSelection === '보' || playerSelection === '바위' && computerSelection === '가위' || playerSelection === '보' && computerSelection === '바위') {
+        console.log('이김', `나:${playerSelection} 컴퓨터:${computerSelection}`);
         playerScore++;
-        console.log(`You Win! ${player} beats ${computerSelection}`);
-        return `You Win! ${player} beats ${computerSelection}`;
-    }
-}
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock Paper Scissors?");
-        const computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-    }
-    if (playerScore === computerScore) {
-        console.log("Result: it's tie");
-    } else if (playerScore > computerScore) {
-        console.log(`Result: You Win! You:${playerScore} Computer:${computerScore}`);
     } else {
-        console.log(`Result: You Lose! Computer:${computerScore} You:${playerScore} `);
+        console.log('짐', `나:${playerSelection} 컴퓨터:${computerSelection}`);
+        computerScore++;
     }
-}
+};
+
+const getResult = () => {
+    console.log(playerScore, computerScore);
+    if (playerScore === computerScore) {
+        console.log('컴퓨터랑 결국 비김');
+    } else if (playerScore > computerScore) {
+        console.log('내가 결국 이김.');
+    } else {
+        console.log('컴퓨터가 결국 이김.');
+    }
+};
+
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = prompt('가위 바위 보를 선택하세요.');
+        const computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+    }
+    getResult();
+};
 
 game();
