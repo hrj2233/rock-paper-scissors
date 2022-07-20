9a51bbadcbf09fb72254e1718e90a1562249ca0d
hrj2233 <hrj2233@gmail.com>
2022-07-20 12:45:53 +0900

    Add UI in styles.css  and Refactor code in main.js

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const winner = document.querySelector('.winner');
+const progress = document.querySelector('.progress');
+
 let playerScore = 0;
 let computerScore = 0;
+let playerChoice = '';
+
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    playerChoice = e.target.textContent;
+    game(playerChoice);
+
+}));
+
+
 
 const getComputerChoice = () => {
     const choice = Math.floor(Math.random() * 3);
@@ -22,34 +36,40 @@ const getComputerChoice = () => {
 
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
-        console.log('비김', `나:${playerSelection} 컴퓨터:${computerSelection}`);
+        progress.textContent = `비김 나:${playerSelection} 컴퓨터:${computerSelection}`;
+        result.innerHTML = `나: ${playerScore} 컴퓨터: ${computerScore}`;
     } else if (playerSelection === '가위' && computerSelection === '보' || playerSelection === '바위' && computerSelection === '가위' || playerSelection === '보' && computerSelection === '바위') {
-        console.log('이김', `나:${playerSelection} 컴퓨터:${computerSelection}`);
+        progress.textContent = `이김 나:${playerSelection} 컴퓨터:${computerSelection}`;
         playerScore++;
+        result.innerHTML = `나: ${playerScore} 컴퓨터: ${computerScore}`;
     } else {
-        console.log('짐', `나:${playerSelection} 컴퓨터:${computerSelection}`);
+        progress.textContent = `짐 나:${playerSelection} 컴퓨터:${computerSelection}`;
         computerScore++;
+        result.innerHTML = `나: ${playerScore} 컴퓨터: ${computerScore}`;
     }
 };
 
 const getResult = () => {
     console.log(playerScore, computerScore);
     if (playerScore === computerScore) {
-        console.log('컴퓨터랑 결국 비김');
+        winner.textContent = '컴퓨터랑 결국 비김';
     } else if (playerScore > computerScore) {
-        console.log('내가 결국 이김.');
+        winner.textContent = '내가 결국 이김.';
     } else {
-        console.log('컴퓨터가 결국 이김.');
+        winner.textContent = '컴퓨터가 결국 이김.';
     }
 };
 
-const game = () => {
-    for (let i = 0; i < 5; i++) {
-        const playerChoice = prompt('가위 바위 보를 선택하세요.');
-        const computerChoice = getComputerChoice();
-        playRound(playerChoice, computerChoice);
-    }
-    getResult();
+const reset = () => {
+    playerScore = 0;
+    computerScore = 0;
 };
 
-game();
+const game = (playerChoice) => {
+    const computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+    if (playerScore === 5 || computerScore === 5) {
+        getResult();
+        reset();
+    }
+};
